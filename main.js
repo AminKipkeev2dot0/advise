628401c3c8ffb2e72e0e59ae0de9e66764a3d5ff
AminKipkeev2dot0 <aminkipkeev12@gmail.com>
2022-01-28 22:18:11 +0300

    ready adaptive home page

---
+++ main.js
@@ -0,0 +1,11 @@
+$('.open-popup').click(function(e){
+    e.preventDefault();
+    $('.popup-bg').fadeIn(400);
+    $('html').addClass('no-scroll');
+});
+
+$('.close-popup').click(function(e){
+    e.preventDefault();
+    $('.popup-bg').fadeOut(300);
+    $('.popup-bg').addClass('close');
+});
